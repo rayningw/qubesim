85247fb587a625f40c0cc5fcc4dabe0a543276f1
Ray Wu <rayning.wu@gmail.com>
2017-02-11 14:13:13 +1100

    Ask for how many drinks

---
+++ src/dimensions.js
@@ -0,0 +1,59 @@
+//@flow
+
+export type Dimension = {
+  name: string,
+  options: Array<string>,
+  defaultOption?: string,
+}
+
+export const dimensions = [
+  {
+    name: 'recipe',
+    options: [
+      'latte', 'flat white', 'cappuccino', 'mocha', 'long black', 'macchiato', 'piccolo', 'espresso',
+      'babycinno',
+    ],
+  },
+  {
+    name: 'size',
+    options: [
+      'regular', 'large',
+    ],
+    defaultOption: 'regular',
+  },
+  {
+    name: 'strength',
+    options: [
+      'regular', 'weak', 'strong',
+    ],
+    defaultOption: 'regular',
+  },
+  {
+    name: 'bean',
+    options: [
+      'house', 'decaf', 'guest',
+    ],
+    defaultOption: 'house',
+  },
+  {
+    name: 'sweetener',
+    options: [
+      'none', '1 sugar', '2 sugars', '3 sugars', '1 equal', '2 equal',
+    ],
+    defaultOption: 'none',
+  },
+  {
+    name: 'dine in / takeaway',
+    options: [
+      'takeaway', 'dine in',
+    ],
+    defaultOption: 'takeaway',
+  },
+  {
+    name: 'warm / iced',
+    option: [
+      'warm', 'on ice',
+    ],
+    defaultOption: 'warm',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 //@flow
 
+import P from 'bluebird';
+import Readline from 'readline';
+
+import type { Dimension } from "./dimensions";
+
 console.log("qubesim");
+
+askForNumberOfDrinks().then(numberOfDrinks => {
+  console.log("Generating " + numberOfDrinks + " drinks");
+});
+
+function askForNumberOfDrinks() {
+  const readline = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new P((resolve, reject) => {
+    readline.question("How many drinks? ", answer => {
+      try {
+        resolve(parseInt(answer));
+      } catch(e) {
+        console.log("Sorry, that is not a number");
+        reject(new Error("Could not parse answer"));
+      }
+      readline.close();
+    });
+  });
+}
